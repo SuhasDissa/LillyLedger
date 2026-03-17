e90730c5fbdd9f8d73e109400c008d1711237127
Suhas Dissanayake <suhasdissa@gmail.com>
2026-03-17 19:55:12 +0530

    feat: add datastructre to store the order

---
+++ include/core/enum/instrument.h
@@ -0,0 +1,14 @@
+#ifndef INSTRUMENT_H
+#define INSTRUMENT_H
+
+#include <cstdint>
+
+enum class Instrument: uint8_t{
+    Rose,
+    Lavender,
+    Lotus,
+    Tulip,
+    Orchid
+};
+
+#endif // INSTRUMENT_H
--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,16 @@
 #include <iostream>
+#include "core/order.h"
 
 using namespace std;
 
 int main(){
-    cout << "LillyLedger" << endl;
+    Order testorder = {
+        "ord1",
+        10.5,
+        100,
+        Instrument::Rose,
+        Side::Buy
+    };
+    cout << "Client Order ID: " << testorder.clientOrderId << endl;
     return 0;
 }
